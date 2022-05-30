40d9f7d9896bab4b6a8ef9e1e32e8b634b48d796
Yaron Kraushar <yaron.kr@outlook.com>
2022-05-30 16:14:03 +1000

    Creating models, routes, and controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,33 @@
+class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+    @car = Car.find(params[:car_id])
+  end
+
+  def create
+    @car = Car.find(params[:car_id])
+    @booking = Booking.new(list_params)
+    @booking.car =  @car
+    @booking.user = current_user
+    if @booking.save
+      redirect_to car_path(@car), notice: 'You are successful'
+    else
+      render :new
+    end
+  end
+
+  def car_bookings
+    @bookings = current_user.car_bookings
+  end
+
+  def my_bookings
+    @booking = current_user.my_bookings
+  end
+
+  private
+
+  def list_params
+    params.require(:booking).permit(:start_date, :end_date, :car_id)
+  end
+end
---
+++ app/models/car.rb
@@ -0,0 +1,10 @@
+class Car < ApplicationRecord
+  belongs_to :user  #
+  has_many :bookings, dependent: :destroy
+  #has_many :users, through: :bookings
+
+  validates :make, presence: true
+  validates :model, presence: true
+  validates :price_day, numericality: { only_integer: true }
+
+end
