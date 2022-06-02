3812b293d57e53aa4f01f9f3061edf27e3e33747
Yaron Kraushar <yaron.kr@outlook.com>
2022-06-02 13:50:20 +1000

    Creating seed file for 1 car

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 require 'date'
+require "open-uri"
 
 s_date1 = Date.new(2022,5,5)
 e_date1 = Date.new(2022,5,10)
@@ -20,32 +21,30 @@ e_date2 = Date.new(2022,6,2)
 
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Booking.destroy_all
 User.destroy_all
 
 yaron2 = User.create!(email: "yaron.kr@test.com", password: "yaron123")
 yujia = User.create!(email: "yujia@test.com", password: "yujia123")
-# julienC = User.create!(email: "julienC@test.com", password: "julienC")
-# julienV = User.create!(email: "julienV@test.com", password: "julienV")
+julienC = User.create!(email: "julienC@test.com", password: "julienC")
+julienV = User.create!(email: "julienV@test.com", password: "julienV")
 
 
-car3 = Car.new(make: "Mitsubishi", model: "2022", price_day: 175, address: "16 Villa Gaudelet, Paris", url: "https://cdn.vroomvroomvroom.com.au/images/vroomvroomvroom-com-au/cms/wagon.jpg")
 
-car3.user = yaron2
-car3.save!
+# EXAMPLPE HOW TO CREATE 1 CAR - COPY IMAGES AND DETAILS FROM THIS WEBSITE: https://www.drivemycar.com.au/cars/toyota-yaris-ashburton/15631
+file = URI.open('https://images.carly.co/15631/15631-front-1-normal.jpg')
 
-car2 = Car.new(make: "Golf", model: "2020", price_day: 75, url:)
-car2.user = yujia
-car2.save!
+description = "My Yaris is great for comfort, speed and economical for driving. It has taken me on very long drives, using only $50 of petrol. To fill the tank, it costs around $40-46. In addition, there is bluetooth for GPS, phone calls and great music. The sound system is epic. In addition, the back seat is comfortable and spacious. The front is as well. Air conditioning works perfectly so does the heater."
 
-Booking.destroy_all
+car1 = Car.new(make: "Toyota Yaris ", model: "Yaris YR", year: 2014,description: description, price_day: 175, address: "16 Villa Gaudelet, Paris")
 
-bookin1 = Booking.new(start_date: s_date1, end_date: e_date1, total_price: 250)
-bookin1.car = car2
-bookin1.user = yaron2
-bookin1.save!
+car1.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+car1.user = yujia
+car1.save!
+# ***************
 
-
-bookin2 = Booking.new(start_date: s_date2, end_date: e_date2, total_price: 150)
-bookin2.car = car3
-bookin2.user = yujia
-bookin2.save!
+# EXAMPLPE HOW TO CREATE BOOKING
+# bookin2 = Booking.new(start_date: s_date2, end_date: e_date2)
+# bookin2.car = car3
+# bookin2.user = yujia
+# bookin2.save!
