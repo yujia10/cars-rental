6b45dc6d160159fe30aa25ea8cddb0ae9f664bf7
Yujia Gu <yujiagu1010@gmail.com>
2022-06-02 13:46:02 +0930

    added markers

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ s_date2 = Date.new(2022,6,1)
 e_date2 = Date.new(2022,6,2)
 
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Booking.destroy_all
 User.destroy_all
 
 yaron2 = User.create!(email: "yaron.kr@test.com", password: "yaron123")
@@ -27,23 +27,21 @@ yujia = User.create!(email: "yujia@test.com", password: "yujia123")
 # julienC = User.create!(email: "julienC@test.com", password: "julienC")
 # julienV = User.create!(email: "julienV@test.com", password: "julienV")
 
-
-car3 = Car.new(make: "Mitsubishi", model: "2022", price_day: 175, address: "16 Villa Gaudelet, Paris", url: "https://cdn.vroomvroomvroom.com.au/images/vroomvroomvroom-com-au/cms/wagon.jpg")
+car3 = Car.new(make: "Mitsubishi", model: "2022", price_day: 175, address: "16 Villa Gaudelet, Paris")
 
 car3.user = yaron2
 car3.save!
 
-car2 = Car.new(make: "Golf", model: "2020", price_day: 75, url:)
+car2 = Car.new(make: "Golf", model: "2020", price_day: 75, address: "37 Thyer Rd, Penfield Gardens")
 car2.user = yujia
 car2.save!
 
-Booking.destroy_all
+
 
 bookin1 = Booking.new(start_date: s_date1, end_date: e_date1, total_price: 250)
 bookin1.car = car2
 bookin1.user = yaron2
 bookin1.save!
-
 
 bookin2 = Booking.new(start_date: s_date2, end_date: e_date2, total_price: 150)
 bookin2.car = car3
